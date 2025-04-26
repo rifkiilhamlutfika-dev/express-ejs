b7eecdd4ba40ae433728e733e405c124b9fc0fb7
rifkiilhamlutfika-dev <rifkiilhamlutfika.dev@gmail.com>
2025-04-26 15:21:23 +0700

    configuration views folder to handling error

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+const path = require("path");
 const express = require("express");
 const port = 8080 || 3000;
 
 const app = express();
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
   res.render("home");

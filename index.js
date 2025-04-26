f567ca969fbf27f6b69c9a27a0959f5a171a7362
rifkiilhamlutfika-dev <rifkiilhamlutfika.dev@gmail.com>
2025-04-26 15:42:44 +0700

    parsing data pt.2

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
+app.get("/t/:tag", (req, res) => {
+  const { tag } = req.params;
+  res.render("tag", { tag });
+});
+
 app.get("/rand", (req, res) => {
   const number = Math.floor(Math.random() * 10) + 1;
   //   res.render("random", { random: number }); that's correct
